0c6532cb07768a2be9955a0b34819bb939f8672f
gaurav_sen0007 <sgaurav0999@gmail.com>
2019-10-21 23:22:16 +0530

    project initiated in gaurav pc

--- modules/admin/pending_bo_request.php
+++ modules/admin/pending_bo_request.php
@@ -4,10 +4,72 @@ $id=$_POST['id'];
 $request_userdata_query="SELECT * FROM boatowner WHERE id=".$id;
 $execute_request_userdata=mysqli_query($connect,$request_userdata_query);
 $result=mysqli_fetch_assoc($execute_request_userdata);
-echo"<pre>";
-print_r($result);
-echo"</pre>";
+//echo"<pre>";
+//print_r($result);
+//echo"</pre>";
+?>
+<div class="row">
+<div class="col"><label>User Name</label>
+<p><?= $result['username'] ?></p></div>
+<div class="col"><label>Name</label>
+<p><?= $result['name'] ?></p></div>
+<div class="col"><label>Email</label>
+<p><?= $result['email'] ?></p></div>
+</div>
 
+<div class="row">
+<div class="col"><label>Adhaar Number</label>
+<p><?= $result['adhaarnumber'] ?></p></div>
+<div class="col"><label>PAN Number</label>
+<p><?= $result['pannumber'] ?></p></div>
+<div class="col"><label>Mobile Number</label>
+<p><?= $result['mobilenumber'] ?></p></div>
+</div>
+
+<div class="row">
+<div class="col"><label>Gender</label>
+<p><?= $result['gender']==1 ? "Male" : "";?>
+    <?=  $result['gender']== 2 ? "Female" : "";?>
+   <?= $result['gender']== 3 ? "other" : ""; ?></p></div>
+<div class="col"><label>Bank Name</label>
+<p><?= $result['bankname'] ?></p></div>
+<div class="col"><label>IFSC Number</label>
+<p><?= $result['ifsc'] ?></p></div>
+</div>
+
+<div class="row">
+<div class="col"><label>Account Name</label>
+<p><?= $result['accountname'] ?></p></div>
+<div class="col"><label>Account Number</label>
+<p><?= $result['accountnumber'] ?></p></div>
+<div class="col"><label>Permanent Address</label>
+<p><?= $result['addressline1'] ?></p></div>
+</div>
+
+<div class="row">
+<div class="col"><label>Correspondence Address</label>
+<p><?= $result['addressline2'] ?></p></div>
+<div class="col"><label>City</label>
+<p><?= $result['cityid'] ?></p></div>
+<div class="col"><label>State</label>
+<p><?= $result['stateid'] ?></p></div>
+</div>
+
+<div class="row">
+<div class="col"><label>Country</label>
+<p><?= $result['countryid'] ?></p></div>
+<div class="col"><label>Zip Code</label>
+<p><?= $result['zipcode'] ?></p></div>
+    <div class="col"></div>
+</div>
+
+
+
+
+
+
+
+<?php
 
 echo"<button type='submit' class='btn btn-dark' onclick='approve(".$result["id"].")'>Approve Request</button>";
 echo"<button type='submit' class='btn btn-dark' onclick='decline(".$result["id"].")'>Decline Request</button>";
